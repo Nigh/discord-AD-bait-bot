2660cb9e6eefeaf3e2da4f79d7fdc52de0b94bb5
xianii <xianyi.xia@outlook.com>
2024-06-10 01:38:55 +0800

    first work version

--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,27 @@
 module app
 
 go 1.21
+
+require (
+	github.com/Nigh/kuma-push v0.1.0
+	github.com/bwmarrin/discordgo v0.28.1
+	github.com/charmbracelet/lipgloss v0.11.0
+	github.com/charmbracelet/log v0.4.0
+	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975
+)
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/x/ansi v0.1.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- golang/main.go
+++ golang/main.go
@@ -1,34 +1,176 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	_ "time/tzdata"
+
+	kuma "github.com/Nigh/kuma-push"
+	"github.com/bwmarrin/discordgo"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	scribble "github.com/nanobox-io/golang-scribble"
 )
 
-var buildTime string
-var startTime string
-var envName string
+var (
+	gKumaPushURL   string
+	gToken         string
+	gBaitChannelID string
+)
 
-func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<html><head><title>HELLO</title><meta http-equiv=\"refresh\" content=\"1\"></head>\n")
+func kumaInit() {
+	k := kuma.New(gKumaPushURL)
+	k.Start()
+}
 
-	fmt.Fprintf(w, "<body><h1>hello</h1>\n")
-	fmt.Fprintf(w, "<table>\n")
-	fmt.Fprintf(w, "<tr><td>env name</td><td>%s</td></tr>\n", envName)
-	fmt.Fprintf(w, "<tr><td>build time</td><td>%s</td></tr>\n", buildTime)
-	fmt.Fprintf(w, "<tr><td>start time</td><td>%s</td></tr>\n", startTime)
-	fmt.Fprintf(w, "<tr><td>current time</td><td>%s</td></tr>\n", time.Now().Format("20060201-150405"))
-	fmt.Fprintf(w, "</table></body>\n</html>")
+var msgLog *log.Logger
+var errLog *log.Logger
+
+func logInit() {
+	_, debug := os.LookupEnv("DEBUG")
+	if debug {
+		log.SetReportCaller(true)
+		log.SetLevel(log.DebugLevel)
+	}
+	log.SetTimeFormat(time.TimeOnly)
+	msgLog = log.WithPrefix("on msg")
+	errLog = log.NewWithOptions(os.Stderr, log.Options{
+		ReportTimestamp: true,
+		ReportCaller:    true,
+		TimeFormat:      "15:04:05.999999999",
+	})
+	styles := log.DefaultStyles()
+	styles.Levels[log.ErrorLevel] = lipgloss.NewStyle().
+		SetString("ERROR").
+		Padding(0, 1, 0, 1).
+		Background(lipgloss.Color("204")).
+		Foreground(lipgloss.Color("0"))
+	styles.Levels[log.FatalLevel] = lipgloss.NewStyle().
+		SetString("FATAL").
+		Padding(0, 1, 0, 1).
+		Background(lipgloss.Color("1")).
+		Foreground(lipgloss.Color("0"))
+
+	errLog.SetStyles(styles)
+}
+
+type testenv struct {
+	Token         string `json:"token"`
+	KumaURL       string `json:"kumaurl"`
+	BaitChannelID string `json:"baitchannelid"`
+}
+
+var testEnv testenv
+
+var db *scribble.Driver
+
+func envInit() {
+	db, _ = scribble.New("../db", nil)
+	db.Read("test", "env", &testEnv)
+	if testEnv.Token != "" {
+		gToken = testEnv.Token
+		gBaitChannelID = testEnv.BaitChannelID
+		gKumaPushURL = testEnv.KumaURL
+	} else {
+		gToken = os.Getenv("BOT_TOKEN")
+		gBaitChannelID = os.Getenv("BAIT_CHANNEL")
+		gKumaPushURL = os.Getenv("KUMA_PUSH_URL")
+	}
+	log.Debug("Read ENV", "gToken", gToken, "gBaitChannelID", gBaitChannelID, "gKumaPushURL", gKumaPushURL)
+}
+
+func init() {
+	logInit()
+	envInit()
+	kumaInit()
+	msgCacheInit()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	log.Info("Bot is ready!")
+}
+
+type Msg struct {
+	ChannelID string
+	MessageID string
+	UserID    string
+}
+
+type MsgCache struct {
+	Msg
+	TimeoutSec int
+}
+
+var msgCache []MsgCache
+
+func msgCacheInit() {
+	msgCache = make([]MsgCache, 0)
+	go msgCacheTimer()
+}
+
+func msgCacheTimer() {
+	interval := time.NewTicker(1 * time.Second)
+	defer interval.Stop()
+	for range interval.C {
+		newMsgCache := make([]MsgCache, 0)
+		for i := range msgCache {
+			msgCache[i].TimeoutSec--
+			if msgCache[i].TimeoutSec > 0 {
+				newMsgCache = append(newMsgCache, msgCache[i])
+			}
+		}
+		msgCache = newMsgCache
+	}
+}
+
+func msgCacheAdd(m *discordgo.MessageCreate) {
+	msgCache = append(msgCache, MsgCache{Msg: Msg{ChannelID: m.ChannelID, MessageID: m.ID, UserID: m.Author.ID}, TimeoutSec: 60})
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	msgLog.Info("New message", "uid", m.Author.ID, "name", m.Author.Username, "channel", m.ChannelID)
+	msgCacheAdd(m)
+	if m.ChannelID == gBaitChannelID {
+		msgLog.Warn("ADS catched", "uid", m.Author.ID, "content", m.Content)
+		err := s.GuildBanCreateWithReason(m.GuildID, m.Author.ID, "Ads bot", 0)
+		if err != nil {
+			errLog.Error("GuildBanCreateWithReason", "err", err)
+		}
+		newMsgCache := make([]MsgCache, 0)
+		for i := range msgCache {
+			if msgCache[i].UserID == m.Author.ID {
+				log.Info("Delete message", "channel", msgCache[i].ChannelID, "message", msgCache[i].MessageID)
+				err = s.ChannelMessageDelete(msgCache[i].ChannelID, msgCache[i].MessageID)
+				if err != nil {
+					newMsgCache = append(newMsgCache, msgCache[i])
+					errLog.Error("ChannelMessageDelete", "channel", msgCache[i].ChannelID, "message", msgCache[i].MessageID, "err", err)
+				}
+			} else {
+				newMsgCache = append(newMsgCache, msgCache[i])
+			}
+		}
+		msgCache = newMsgCache
+	}
 }
 
 func main() {
-	envName = os.Getenv("NAME")
+	dg, err := discordgo.New("Bot " + gToken)
+	if err != nil {
+		errLog.Fatal("error creating Discord session", "err", err)
+	}
 
-	startTime = time.Now().Format("20060201-150405")
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":11180", nil)
+	dg.AddHandler(ready)
+	dg.AddHandler(messageCreate)
+	err = dg.Open()
+	if err != nil {
+		errLog.Fatal("error opening connection", "err", err)
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	dg.Close()
+	log.Info("Bot OFFLINE")
 }
